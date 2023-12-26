05ba5ea9a917f61a18f4e300f253dc17c5fa3d7f
Ceren <Ceren@LAPTOP-T6B996G7>
2023-12-26 10:58:42 +0300

    Add project files.

---
+++ DAL/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL  //Data Access Layer
+{
+    public class Helper
+    {
+        SqlConnection baglanti;
+        SqlCommand cmd;
+
+        //SQL Baglanti stringini OkulApp projesindeki App.config dosyasi icine attik. Oradan getiriyoruz.
+        string cstr = ConfigurationManager.ConnectionStrings["cstr"].ConnectionString;
+
+        public int ExecuteNonQuery(string cmdtext, SqlParameter[] p = null)
+        {
+            using (baglanti = new SqlConnection(cstr))
+            {
+                using (cmd = new SqlCommand(cmdtext,baglanti)) 
+                {
+                    if (p != null)
+                    {
+                        cmd.Parameters.AddRange(p);
+                    }
+                    baglanti.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+            //DISPOSE islemi SQL OBJELERINI BELLEKTEN ATAR (normalde garbage collector isimli sistem oto yapar. ancak sql gibi islemleri yapmaz)
+            //using kullandigimizda using in disina cikildiginda otomatik dispose eder bu sayede dispose kodu yazmamiza gerek kalmaz (dogal olarak baglantiyi da oto kapatir) 
+        }
+    }
+}
---
+++ OkulApp.BLL/OgrenciBL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using DAL;
+using OkulApp.MODEL;
+
+namespace OkulApp.BLL //Business Logic Layer
+{
+    public class OgrenciBL
+    {
+        public bool OgrenciEkle(Ogrenci ogr)
+        {
+            try
+            {
+                //cmd.Parameters.AddWithValue("@Ad", ogr.Ad);   Bu sekilde de olabilir. Range de gerek kalmaz
+                SqlParameter[] p =
+                {
+                    new SqlParameter("@Ad", ogr.Ad),
+                    new SqlParameter("@Soyad", ogr.Soyad),
+                    new SqlParameter("@Numara", ogr.Numara)
+                };
+
+                var hlp = new Helper();
+                return hlp.ExecuteNonQuery("Insert into tblOgrenci (Ad,Soyad,Numara) values (@Ad,@Soyad,@Numara)", p) > 0;
+            }
+            catch (SqlException )
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                //if (baglanti != null && baglanti.State != ConnectionState.Closed)
+                //{
+                //    //baglanti.Close();
+                //    baglanti.Dispose();   //SQL OBJELERINI BELLEKTEN ATAR (normalde garbage collector isimli sistem oto yapar. ancak sql islemlerini yapmaz)
+                //    cmd.Dispose();
+                //}
+            }
+        }
+    }
+}
+
+//n KATMANLI MIMARI (3 katmanli proje)
